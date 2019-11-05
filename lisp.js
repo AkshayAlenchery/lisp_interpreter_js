517ef282e02ebccbca27bb9bb1154a4699912282
Akshay Alenchery <akshayalenchery@gmail.com>
2019-11-05 16:21:53 +0530

    Implemented define parser

--- lisp.js
+++ lisp.js
@@ -7,8 +7,12 @@ const globalEnv = {
 
 const numberParser = (inputExp, match = null) => (match = inputExp.match(/^-?(0|[1-9][0-9]*)(\.[0-9]+)?((e|E)(-|\+)?[0-9]+)?/)) === null ? null : [match[0] * 1, inputExp.slice(match[0].length).trim()]
 
-const operatorParser = (inputExp, env) => {
-  if (env[inputExp[0]] === undefined) return null
+const identifierParser = (inputExp, match = null) => (match = inputExp.match(/^[a-zA-Z]*/)) === null ? null : [match[0], inputExp.slice(match[0].length).trim()]
+
+const findInEnv = variable => globalEnv[variable] === undefined ? null : globalEnv[variable]
+
+const operatorParser = (inputExp) => {
+  if (globalEnv[inputExp[0]] === undefined) return null
   const operator = inputExp[0]
   inputExp = inputExp.slice(1).trim()
   const operands = []
@@ -20,19 +24,40 @@ const operatorParser = (inputExp, env) => {
   }
   if (!inputExp[0].startsWith(')')) return null
   inputExp = inputExp.slice(1).trim()
-  return [env[operator](operands), inputExp]
+  return [globalEnv[operator](operands), inputExp]
+}
+
+const defineParser = inputExp => {
+  if (!inputExp.startsWith('define')) return null
+  inputExp = inputExp.slice(6).trim()
+  const varName = inputExp.slice(0, inputExp.indexOf(' ')).trim()
+  const checkVar = identifierParser(varName)
+  if (!checkVar) return null
+  if (checkVar[0] !== varName) return null
+  inputExp = inputExp.slice(varName.length).trim()
+  const varValue = numberParser(inputExp)
+  if (!varValue) return null
+  inputExp = varValue[1]
+  if (!inputExp.startsWith(')')) return null
+  globalEnv[varName] = varValue[0]
+  inputExp = inputExp.slice(1)
+  return [varName, inputExp]
 }
 
 const expressionParser = inputExp => {
   if (!inputExp.startsWith('(')) return null
   inputExp = inputExp.slice(1).trim()
-  const result = operatorParser(inputExp, globalEnv)
+  const result = operatorParser(inputExp)
   if (!result) return null
   return result
 }
 
 const specialFormParser = inputExp => {
-  return null
+  if (!inputExp.startsWith('(')) return null
+  inputExp = inputExp.slice(1).trim()
+  const result = defineParser(inputExp)
+  if (!result) return null
+  return result
 }
 
 const evaluator = inputExp => {
@@ -40,8 +65,12 @@ const evaluator = inputExp => {
   let result = specialFormParser(inputExp) || expressionParser(inputExp)
   if (result) return result
   result = numberParser(inputExp)
+  if (result) return result
+  result = identifierParser(inputExp)
   if (!result) return null
-  return result
+  const varVal = findInEnv(result[0])
+  if (!varVal) return null
+  return [varVal, result[1]]
 }
 
 const lispEval = inputExp => {
@@ -51,5 +80,6 @@ const lispEval = inputExp => {
   return lispEval(result[1])
 }
 
-const input = '( + 1 2 3 ( - 1 2 3 ))'
+const input = '(define ak 100) (* ak ak)'
 console.log(lispEval(input))
+console.log(globalEnv)
